bef98b4dcd50801ea88ddad73fb48753d4b56cda
RocktimSaikia <srocktim61@gmail.com>
2019-06-02 13:05:57 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Header from "./components/layouts/Header";
+import Footer from "./components/layouts/Footer";
+import News from "./components/News";
+import "./App.css";
+import axios from "axios";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [list, setList] = useState([]);
 
+  useEffect(() => {
+    getData().then(arr => getDeatils(arr).then(item => setList(item)));
+  }, []);
+
+  const getData = async function() {
+    const arr = [];
+    try {
+      const { data } = await axios.get(
+        `https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`
+      );
+      data.slice(0, 5).map(item => arr.push(item));
+    } catch (error) {
+      return error;
+    }
+    return arr;
+  };
+
+  const getDeatils = async function(arr) {
+    const promises = arr.map(async item => {
+      const { data } = await axios.get(
+        `https://hacker-news.firebaseio.com/v0/item/${item}.json?print=pretty`
+      );
+      return {
+        author: data.by,
+        title: data.title,
+        score: data.score,
+        comments_count: data.descendants,
+        time: data.time,
+        url: data.url
+          .replace("http://", "")
+          .replace("https://", "")
+          .split(/[/?#]/)[0]
+      };
+    });
+    const results = await Promise.all(promises);
+    return results;
+  };
+
+  return (
+    <div>
+      <Header />
+      <News list={list} />
+      <Footer />
+    </div>
+  );
+};
 export default App;
---
+++ src/components/News.js
@@ -0,0 +1,52 @@
+import React from "react";
+import timeago from "epoch-timeago";
+
+const News = ({ list }) => {
+  return (
+    <div className="container-fluid main">
+      <table>
+        <tbody>
+          {list.map(
+            ({ author, title, score, comments_count, time, url }, index) => (
+              <tr>
+                <td>{index + 1}</td>
+                <td style={{ padding: "0px" }}>
+                  <i
+                    className="fas fa-sort-up"
+                    style={{
+                      fontSize: "30px",
+                      marginTop: "16px",
+                      padding: "0px",
+                      marginRight: "0px"
+                    }}
+                  />
+                </td>
+                <td style={{ padding: "0px", paddingRight: "20px" }}>
+                  {" "}
+                  {score}
+                </td>
+                <td style={{ paddingRight: "100px", fontWeight: "600" }}>
+                  {title}
+                </td>
+                <td>
+                  <i className="fas fa-user" /> {author}
+                </td>
+                <td>
+                  <i className="fas fa-globe" /> {url}
+                </td>
+                <td>
+                  <i className="fas fa-clock"> {timeago(time * 1000)}</i>
+                </td>
+                <td>
+                  <i className="far fa-comment-alt" /> {comments_count}
+                </td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default News;
