abac6287ac16506e0deb5c65bc2246a58826ea0e
RocktimSaikia <rocktimthedev@gmail.com>
2021-03-30 04:46:04 -1100

    style: add reply button

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,26 +1,35 @@
 import React from "react";
-import dayjs from "dayjs";
-import localizedFormat from "dayjs/plugin/localizedFormat";
+import * as timeago from "timeago.js";
+import ReplyIcon from "./icons/reply";
 
 export default function Comments({ comments }) {
-  dayjs.extend(localizedFormat);
+  const [showReplies, setShowReplies] = React.useState(false);
+
   return (
     <div>
       <ul>
         {comments.map((comment, i) => (
           <li className="my-6 font-inter" key={i}>
             <div>
-              <span className="text-gray-800 text-sm font-bold mr-4 mb-2 inline-block">
+              <span className="text-gray-800 text-xs font-bold mr-4 mb-2 inline-block">
                 {comment.author}
               </span>
               <span className="text-gray-500 text-xs">
-                {dayjs(comment.created_at).format("MMM D, h:mm A")}
+                {timeago.format(comment.created_at)}
               </span>
             </div>
             <p
-              className="text-sm text-gray-800 font-normal tracking-tight"
+              className="comments-paragraph"
               dangerouslySetInnerHTML={{ __html: comment.text }}
             ></p>
+            {comment["children"].length > 0 && (
+              <button
+                className="flex items-center text-xs underline mt-2"
+                onClick={() => setShowReplies(!showReplies)}
+              >
+                <ReplyIcon /> <span>replies...</span>
+              </button>
+            )}
           </li>
         ))}
       </ul>
