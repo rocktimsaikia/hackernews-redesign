efefffe9e513ffe4902dba442c3427779adb1df4
RocktimSaikia <rocktimthedev@gmail.com>
2021-03-30 17:41:56 -1100

    feat: complete the comment/reply ui

---
+++ components/Comment.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import * as timeago from "timeago.js";
+import ReplyIcon from "./icons/reply";
+
+export default function Comment({ comment }) {
+  const [showReplies, setShowReplies] = React.useState(false);
+
+  return (
+    <li className="my-6 font-inter">
+      <div>
+        <span className="text-gray-800 text-xs font-bold mr-4 mb-2 inline-block">
+          {comment.author}
+        </span>
+        <span className="text-gray-500 text-xs">
+          {timeago.format(comment.created_at)}
+        </span>
+      </div>
+      <p
+        className="comments-paragraph"
+        dangerouslySetInnerHTML={{ __html: comment.text }}
+      ></p>
+      {comment["children"].length > 0 && (
+        <button
+          className="flex items-center text-xs underline mt-2 text-gray-600"
+          onClick={() => setShowReplies(!showReplies)}
+        >
+          <ReplyIcon /> <span>replies...</span>
+        </button>
+      )}
+      {showReplies && (
+        <div className="pl-4 border-l-2">
+          {comment.children.map((reply) => (
+            <Comment comment={reply} />
+          ))}
+        </div>
+      )}
+    </li>
+  );
+}
--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,36 +1,12 @@
 import React from "react";
-import * as timeago from "timeago.js";
-import ReplyIcon from "./icons/reply";
+import Comment from "./Comment";
 
 export default function Comments({ comments }) {
-  const [showReplies, setShowReplies] = React.useState(false);
-
   return (
     <div>
       <ul>
         {comments.map((comment, i) => (
-          <li className="my-6 font-inter" key={i}>
-            <div>
-              <span className="text-gray-800 text-xs font-bold mr-4 mb-2 inline-block">
-                {comment.author}
-              </span>
-              <span className="text-gray-500 text-xs">
-                {timeago.format(comment.created_at)}
-              </span>
-            </div>
-            <p
-              className="comments-paragraph"
-              dangerouslySetInnerHTML={{ __html: comment.text }}
-            ></p>
-            {comment["children"].length > 0 && (
-              <button
-                className="flex items-center text-xs underline mt-2"
-                onClick={() => setShowReplies(!showReplies)}
-              >
-                <ReplyIcon /> <span>replies...</span>
-              </button>
-            )}
-          </li>
+          <Comment comment={comment} key={i} />
         ))}
       </ul>
     </div>
