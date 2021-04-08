981b27f9eacc6525f44508b91fb79def8b3446f4
RocktimSaikia <rocktimthedev@gmail.com>
2021-04-07 16:53:55 -1100

    refactor: add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import "../styles/globals.css";
 import { useState } from "react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 import Sidebar from "../components/Sidebar";
 import SidebarMobile from "../components/SidebarMobile";
 import Navbar from "../components/Navbar";
@@ -9,6 +12,19 @@ import CoffeeCup from "../components/icons/coffeecup";
 
 function MyApp({ Component, pageProps }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Google analytics
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <div className="relative flex flex-col">
       <div className="bg-overlay" />
